fc65a6f81db492347b1972d6111ed1f0a996737c
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-10-07 14:04:59 +0200

    Putting messages in a flatlist

---
+++ src/constants/chat.ts
@@ -0,0 +1,2 @@
+export const MESSAGE_SENT = 'SENT';
+export const MESSAGE_RECEIVED = 'RECEIVED';
--- src/screens/chat_screen.tsx
+++ src/screens/chat_screen.tsx
@@ -1,19 +1,49 @@
-import { StyleSheet, View } from 'react-native'
-import React from 'react'
-import AppHeader from '../components/app_header'
-import SentMessageCard from '../components/sent_message_card'
-import ResponseMessageCard from '../components/response_message_card'
+import { FlatList, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import AppHeader from '../components/app_header';
+import SentMessageCard from '../components/sent_message_card';
+import ResponseMessageCard from '../components/response_message_card';
+import { MESSAGE_RECEIVED, MESSAGE_SENT } from '../constants/chat';
 
 const ChatScreen = () => {
+  const messageList = [
+    {
+      message: 'Hello AI',
+      id: 1,
+      type: MESSAGE_SENT,
+    },
+    {
+      message: 'Hi, how may I help you today?',
+      id: 2,
+      type: MESSAGE_RECEIVED,
+    },
+    {
+      message: 'Tell me a joke',
+      id: 3,
+      type: MESSAGE_SENT,
+    },
+  ];
+
+  const [messages, setMessage] = useState(messageList);
+
   return (
     <View>
       <AppHeader />
-      <SentMessageCard message='Hello AI'/>
-      <ResponseMessageCard message='Hi, how are you?'/>
+      <FlatList
+        data={messages}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => {
+          return item.type === 'SENT' ? (
+            <SentMessageCard message={item.message} />
+          ) : (
+            <ResponseMessageCard message={item.message} />
+          );
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default ChatScreen
+export default ChatScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
